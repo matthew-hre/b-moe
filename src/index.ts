2279baa10b97de962e33d0e654b9d57812033175
Matthew Hrehirchuk <me@matthew-hre.com>
2026-06-06 07:20:59 -0600

    fix: ignore linting on entry log

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ const PORT = parseInt(process.env.PORT ?? "3000");
 const container = createDiContainer();
 const app = createRoutes(container);
 
+// this is like the one necessary log statement
+// oxlint-disable-next-line
 console.log(`Server running on http://localhost:${PORT}`);
 
 Bun.serve({
